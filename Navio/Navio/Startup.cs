736c8b00a9eb8a1fadcaf36865b34b8f3dcd11b2
Laura <laurahcoto@gmail.com>
2015-06-27 22:03:38 -0600

    .

---
+++ Navio/Navio/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Navio.Startup))]
+namespace Navio
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
